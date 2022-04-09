e05d2f358acc0e4083929b099ca601806f5b44d8
meghanaa3511 <103300123+meghanaa3511@users.noreply.github.com>
2022-04-09 11:04:15 +0530

    meghanaa3511.github.io

---
+++ Length.js
@@ -0,0 +1,7 @@
+function KmtoMConverter(length) {
+    document.querySelector(".meters").innerHTML=length*1000;
+ }
+
+ function MtoKmConverter(length) {
+    document.querySelector(".Kilometers").innerHTML=length/1000;
+ }
